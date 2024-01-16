50aa55bf2897bec1192fc310d27b0062e7e8ec2c
professorDeveloper <azamovhud007@outlook.com>
2024-01-17 01:04:40 +0500

    Hehehe Uzmovi Your CRACKED :DDDD

--- app/src/main/java/com/azamovhudstc/scarpingtutorial/uzmovi/Main.kt
+++ app/src/main/java/com/azamovhudstc/scarpingtutorial/uzmovi/Main.kt
@@ -2,7 +2,10 @@ package com.azamovhudstc.scarpingtutorial.uzmovi
 
 import com.azamovhudstc.scarpingtutorial.utils.Utils
 import com.azamovhudstc.scarpingtutorial.utils.Utils.getJsoup
+import com.azamovhudstc.scarpingtutorial.utils.parser
 import com.azamovhudstc.scarpingtutorial.uzmovi.movie.ParsedMovie
+import com.lagradost.nicehttp.Requests
+import kotlinx.coroutines.runBlocking
 
 private val mainUrl = "http://uzmovi.com/"
 
@@ -17,6 +20,33 @@ fun main() {
     }
     println("----------------SELECTED MOVIE ${list.get(0).title}---------------")
     movieDetails(list.get(4)) /// Add Trailer Scarping
+
+    runBlocking {
+        getM3u8LocationFile()
+
+    }
+}
+
+
+suspend fun getM3u8LocationFile() {
+    val mainUrl =
+        "http://uzmovi.com/play/e4y5z5x543r24484y2u2q254p204x2w2r2u223s2s2033344y544t4y4m4i4t4e4q234x5q5g2f4x5r4l4s5q5w5o4c2w576h2h4j4f582m5c4q2i4l403/"
+    val requests = Requests(Utils.httpClient, responseParser = parser)
+
+    val data = requests.get(
+        mainUrl,
+        headers = mapOf(
+            "Cookie" to "_ym_uid=1664171290829008916; \"_pubcid\"=439b1e7c-eab3-4392-a9a7-19b1e53fe9f3; _ym_d=1696009917; __gads=ID=47342de96a689496-224c06c4fbdd00d6:T=1685651803:RT=1699104092:S=ALNI_Mb2ZhtSMyfS5P7PZrwc7eQv5t2WRg; __gpi=UID=00000c2ace922f58:T=1685651803:RT=1699104092:S=ALNI_MZzapclV2KKmb9oTHGcM6MVmi-EBg; comment_name=Foydalanuvchi; _pbjs_userid_consent_data=3524755945110770; _gid=GA1.2.704416453.1705347575; adrcid=ACr-r0sIPrgrh7iAg-Dg5rQ; adrcid_cd=1705407018194; _ym_isad=1; ci_session=rku1vq97bd4cdr8e1piekobjspkeuedl; _ga_XVBVMVW651=GS1.1.1705431478.202.1.1705433781.0.0.0; _ga=GA1.2.504275464.1685651802",
+            "Connection" to "keep-alive",
+            "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/237.84.2.178 Safari/537.36",
+            "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding" to "gzip, deflate",
+            "DNT" to "1",
+            "host" to "uzmovi.com",
+            "Accept-Language" to "ru-RU,ru;q=0.9,uz-UZ;q=0.8,uz;q=0.7,en-GB;q=0.6,en-US;q=0.5,en;q=0.4",
+        ), referer = "http://uzmovi.com/"
+    )
+    println(data.url)
 }
 
 fun movieDetails(parsedMovie: ParsedMovie) {
